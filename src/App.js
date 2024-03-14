60a34c86ca0b75423cb9a0ec4f4f2683ef736185
Kevin Khong <kevin79ers@gmail.com>
2024-03-14 14:04:13 -0500

    image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           This will eventually be a sentiment analysis webapp.
         </p>
