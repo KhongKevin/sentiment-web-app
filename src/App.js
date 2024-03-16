0e68a2b2ca75d4bc630df4f505a7bf2dfc77e572
Kevin Khong <kevin79ers@gmail.com>
2024-03-15 23:20:29 -0500

    stash 2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SecondaryPage from './pages/SecondaryPage';
-import InitialPage from './pages/InitialPage'
+import InitialPage from './pages/InitialPage';
+import Navbar from './components/NavBar';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <Router>        
+    <Router>  
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<InitialPage />} />
+        <Route path="/InitialPage" element={<InitialPage />} />
+        <Route path="/AboutPage" element={<AboutPage />} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/SecondaryPage" element={<SecondaryPage />} />
       </Routes>
--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -5,15 +5,16 @@ import Button from '@mui/material/Button';
 
 const HomePage = () => {
     return (
-      <div>
-        <h1>Home Page</h1>
-        <Link to="/SecondaryPage">
-          <Button variant="contained" color="primary">Secondary Page</Button>
-        </Link>
-        <Link to="/HomePage">
-          <Button variant="contained" color="primary">HomePage</Button>
-        </Link>
-      </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+            <h1>Initial Page</h1>
+            <Link to="/SecondaryPage">
+                <Button variant="contained" color="primary">Secondary Page</Button>
+            </Link>
+            <Link to="/HomePage">
+                <Button variant="contained" color="primary">HomePage</Button>
+            </Link>
+        </div>
     );
-  };
+};
+
 export default HomePage;
